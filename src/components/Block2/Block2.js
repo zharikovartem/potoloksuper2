867e2321428166e3e997807b67efcb26e51490a7
7383125@mail.ru <7383125@mail.ru>
2020-03-10 13:03:54 +0300

    1

--- src/components/Block2/Block2.js
+++ src/components/Block2/Block2.js
@@ -12,30 +12,40 @@ const UrlItem = (props) => {
     );
 }
 
-const Block2 = () => {
+let Block2 = () => {
     let data = [
-        {name: 'Одноуровневый', to: '/simple'},
-        {name: 'Двухуровневый', to: '/double'}
+        {name: 'Одноуровневый', to: '/simple', rowNumber: '0'},
+        {name: 'Двухуровневый', to: '/double', rowNumber: '0'},
+        {name: 'Глянцевый', to: '/gloss', rowNumber: '1'},
+        {name: 'Матовый', to: '/matt', rowNumber: '1'},
+        {name: 'Сатин', to: '/satin', rowNumber: '1'},
+        {name: 'Тканевый', to: '/cloth', rowNumber: '1'}
     ]
+
+    // формируем массив по map
+    let results = data.map(item => <UrlItem name={item.name} to={item.to} /> );
 
     return (
         <div className={st.Block2}>
-                {/* {`${st.item} ${st.active}`}  - Засунуть 2 класса */}
                 <p className={st.header}>
                     ВЫБЕРИТЕ ОПТИМАЛЬНЫЙ  ТИП ПОТОЛКА
                 </p>
-                <div className={st.types}>
-                    <UrlItem name={data[0].name} to={data[0].to} />
-                    <UrlItem name={data[1].name} to={data[1].to} />
+                <div className={st.types0}>
+                    <UrlItem name={ data[0].name } to={ data[0].to } />
+                    <UrlItem name={ data[1].name } to={ data[1].to } />
 
                 </div>
                 <Route path='/simple' component={Simple}></Route>
-                <Route path='/double' component={Double}></Route>
-                <div className={st.types2}>
-                    <UrlItem name='Глянцевый' to='/gloss' />
-                    <UrlItem name='Матовый' to='/matt' />
-                    <UrlItem name='Сатин' to='/satin' />
-                    <UrlItem name='Тканевый' to='/cloth' />
+
+                {/* передаем props */}
+                <Route path='/double' render={ () => <Double/> } />
+
+                <div className={ st.types1 }>
+                    <UrlItem name={ data[2].name } to={ data[2].to } />
+                    <UrlItem name={ data[3].name } to={ data[3].to } />
+                    <UrlItem name={ data[4].name } to={ data[4].to } />
+                    <UrlItem name={ data[5].name } to={ data[5].to } />
+                    {/* { results } */}
                 </div>
         </div>
     )
