2456bdb5e5cb426d490fbdca5eb211bf54c129b3
7383125@mail.ru <7383125@mail.ru>
2020-03-18 13:41:40 +0300

    1

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import store from './Redux/redux-store';
 
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import StoreContext from './store-context';
 
 let rerenderEntireTree = () => {
     let test = store.getState();
@@ -17,29 +18,31 @@ let rerenderEntireTree = () => {
     return (
         ReactDOM.render(
             <BrowserRouter>
-                <App data={store.getState()}
-                    dispatch={store.dispatch.bind(store)}
-                >
-                </App>
+                <StoreContext.ProviderStoreContext value={store}>
+                    <App data={store.getState()}
+                        dispatch={store.dispatch.bind(store)}
+                    >
+                    </App>
+                </StoreContext.ProviderStoreContext>
             </BrowserRouter>
             , document.getElementById('root'))
     );
 };
 
-// let rerenderOrder = (store) => {
-//     console.log(store);
-//     debugger;
-//     // console.log(store.getState());
-//     return (
-//         ReactDOM.render(
-//             <div>
-//                 {/* Новый заказ от {state.name}. Тел: {orders.phone} */}
-//                 Новый заказ от {store.orderPromo.nameDefault}. Тел: {store.orderPromo.phoneDefault}
-//                 <button onClick={() => rerenderEntireTree(store)}>OK</button>
-//             </div>
-//             , document.getElementById('root'))
-//     );
-// };
+let rerenderOrder = (store) => {
+    console.log(store);
+    debugger;
+    // console.log(store.getState());
+    return (
+        ReactDOM.render(
+            <div>
+                {/* Новый заказ от {state.name}. Тел: {orders.phone} */}
+                Новый заказ от {store.orderPromo.nameDefault}. Тел: {store.orderPromo.phoneDefault}
+                <button onClick={() => rerenderEntireTree(store)}>OK</button>
+            </div>
+            , document.getElementById('root'))
+    );
+};
 
 rerenderEntireTree(store);
 
